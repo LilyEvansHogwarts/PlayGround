c6c9b75aa6f5277fc9c225da5eb8307e964ba9f1
LilyEvansHogwarts <16110720018@fudan.edu.cn>
2018-10-07 20:09:51 +0800

    NAR_GP

--- Multi-fidelity/code/src/GP.py
+++ Multi-fidelity/code/src/GP.py
@@ -13,6 +13,12 @@ def chol_inv(L, y):
     v = np.linalg.solve(L, y)
     return np.linalg.solve(L.T, v)
 
+def RBF(x, xp, hyp, active_dims):
+    output_scale = np.exp(hyp[0])
+    lengthscales = np.exp(hyp[1:])
+    diffs = np.expand_dims((x[active_dims].T/lengthscales).T,2) - np.expand_dims((xp[active_dims].T/lengthscales).T,1)
+    return output_scale * np.exp(-0.5*np.sum(diffs**2,axis=0))
+
 class GP:
     def __init__(self, dataset, bfgs_iter=100, debug=True):
         self.train_x = dataset['train_x']
@@ -31,23 +37,11 @@ class GP:
             theta[2+i] = np.maximum(-100, np.log(0.5*(self.train_x[i].max() - self.train_x[i].min())))
         return theta
 
-    def kernel(self, x, xp, hyp):
-        output_scale = np.exp(hyp[0])
-        lengthscales = np.exp(hyp[1:])
-        diffs = np.expand_dims((x.T/lengthscales).T,2) - np.expand_dims((xp.T/lengthscales).T,1)
-        if self.debug:
-            print('x',x.shape)
-            print('xp',xp.shape)
-            print('expand x',np.expand_dims((x.T/lengthscales).T,2).shape)
-            print('expand xp',np.expand_dims((xp.T/lengthscales).T,1).shape)
-            print('diff.shape',diffs.shape)
-        return output_scale * np.exp(-0.5*np.sum(diffs**2,axis=0))
-
     def neg_likelihood(self, theta):
         sn2 = np.exp(theta[0])
         hyp = theta[1:]
 
-        K = self.kernel(self.train_x, self.train_x, hyp) + sn2*np.eye(self.num_train)
+        K = RBF(self.train_x, self.train_x, hyp, np.arange(self.dim)) + sn2*np.eye(self.num_train)
         L = np.linalg.cholesky(K)
         if self.debug:
             print('K.shape',K.shape)
@@ -107,8 +101,8 @@ class GP:
     def predict(self, test_x):
         sn2 = np.exp(self.theta[0])
         hyp = self.theta[1:]
-        tmp = self.kernel(test_x, self.train_x, hyp)
+        tmp = RBF(test_x, self.train_x, hyp, np.arange(self.dim))
         py = np.dot(tmp, self.alpha)
-        ps2 = sn2 + self.kernel(test_x, test_x, hyp) - np.dot(tmp, chol_inv(self.L, tmp.T))
+        ps2 = sn2 + RBF(test_x, test_x, hyp, np.arange(self.dim)) - np.dot(tmp, chol_inv(self.L, tmp.T))
         return py, ps2
 
