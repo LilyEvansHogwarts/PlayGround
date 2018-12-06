361090af2966b580cd021b11aa97933e23902fb7
LilyEvansHogwarts <16110720018@fudan.edu.cn>
2018-12-06 15:12:14 +0800

    make_figure PA_figure

--- Multi-fidelity/NAR_BO/PA_figure/PA_figure.py
+++ Multi-fidelity/NAR_BO/PA_figure/PA_figure.py
@@ -5,14 +5,21 @@ import matplotlib.pyplot as plt
 for i in range(4,5):
     with open('PA'+str(i)+'.pickle','rb') as f:
         dataset = pickle.load(f)
+
+    print(dataset['x'][0,0])
+    print(dataset['x'][1,0])
+    print(dataset['x'][2,0])
+    print(dataset['x'][3,0])
         
     x = dataset['x'][i]
 
-    plt.plot(x, dataset['low_y'][0], color='gray', label='low-fidelity')
-    plt.plot(x, dataset['high_y'][0], color='black', label='high-fidelity')
+    plt.plot(x, dataset['low_y'][0], label='exact low fidelity', linewidth=2, color='m')#'darkgray')#'lightslategray')
+    plt.plot(x, dataset['high_y'][0], label='exact high fidelity', linewidth=2, color='black')
     #plt.xticks(fontsize=8)
     #plt.xticks(fontsize=8)
-    plt.legend(fontsize=15)
+    plt.xlabel('Vb(V)', {'size':15})
+    plt.ylabel('Eff(%)', {'size':15})
+    plt.legend(fontsize=12)
     plt.show()
     '''
     for j in range(3):
